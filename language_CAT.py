12d2214e41d2ea259b7f1b40e7e4bd75a3b520da
CinS12 <arturmarti12@gmail.com>
2021-03-16 10:15:43 +0100

    Initial commit

---
+++ language_CAT.py
@@ -0,0 +1,8 @@
+"""Script with the GUI text in CATALAN.
+sectionauthor:: Artur Martí Gelonch <artur.marti@students.salle.url.edu>
+WARNING: choose language options still in development (no functional).
+"""
+# Source: (https://ca.wikipedia.org/wiki/Escala_de_Barthel)
+BARTHEL_DESCRIPTION = "Escala ordinal utilitzada per mesurar l'acompliment en activitats de la vida diària bàsiques (AVDB)."
+# Source: (https://www.murciasalud.es/preevid/1103)
+EMINA_DESCRIPTION = "L'escala Emina és un instrument de valoració del risc de desenvolupament d'úlceres de pressió en pacients hospitalitzats."
